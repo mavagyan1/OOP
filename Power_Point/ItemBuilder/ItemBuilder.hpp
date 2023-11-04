208745e38621e2f983a5975247de5b4b166ecc03
armanqalantaryan08 <arman.qalantaryan@teracloud.com>
2023-11-04 22:59:20 +0400

    minor fixes

--- Power_Point/ItemBuilder/ItemBuilder.hpp
+++ Power_Point/ItemBuilder/ItemBuilder.hpp
@@ -1,11 +1,17 @@
 #ifndef  __ITEM_BUILDER_HPP__
 #define  __ITEM_BUILDER_HPP__
 #include "../items/Item.hpp"
+#include "../AttributeRegistry/AttributeRegistry.hpp"
+
 //class Item;
 class IItemBuilder {
 public:
-    virtual Item* buildItem() = 0;
+    virtual Item* buildItem(std::unordered_map<std::string, std::string>) = 0;
     virtual ~IItemBuilder() = default;
+
+private:
+
+    AttributeRegistry* attributeRegistry;
 };
 
 #endif //__ITEM_BUILDER_HPP__
--- Power_Point/ItemBuilder/RectangleItemBuilder.cpp
+++ Power_Point/ItemBuilder/RectangleItemBuilder.cpp
@@ -1,8 +1,12 @@
 #include "RectangleItemBuilder.hpp"
 #include "../items/rectangle.hpp"
+#include <functional>
 
 Item* RectangleItemBuilder::buildItem(std::unordered_map<std::string, std::string> arguments) {
     auto item = new Rectangle{};
+
+    std::for_each(std::begin(arguments), std::end(arguments),)
+    auto attribute = arguments.find()
 
     return item;
 }
