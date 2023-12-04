8c4258665a40c173a7bbc2097bddda2737dbe44e
Mane Avagyan <mane.avagyan@teracloud.com>
2023-12-04 19:21:12 +0400

    Change the RGB888 to RGBA8888

--- Power_Point/AttributeBuilder/FillColorAttributeSetter.cpp
+++ Power_Point/AttributeBuilder/FillColorAttributeSetter.cpp
@@ -5,16 +5,16 @@
 
  void FillColorAttributeSetter::setAttribute(Item* item, std::string rgbStream) {
     std::istringstream color_stream(rgbStream);
-    int r,g,b;
+    int r,g,b,a;
     char comma;
-    color_stream >> r >> comma >> g >> comma >> b;
+    color_stream >> r >> comma >> g >> comma >> b >> comma >> a;
 
-    if (color_stream.fail() || comma != ',' || color_stream.rdbuf()->in_avail() != 0) {
+    if (color_stream.fail() || comma != ',' || color_stream.rdbuf()->in_avail() != 0)
         throw std::runtime_error("Faild to read the fill color attribute");
     
-    int rgb = (r << 16) | (g << 8) | b;
+    int rgba = (r << 24) | (g << 16) | (b << 8) | a;
 
     
-    item->setFillColour(rgb);
- }
+    item->setFillColour(rgba);
+}
 
